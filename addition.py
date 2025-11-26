568e785d2a5f78feaf5e6e7fe31076be0248cf22
Mohammadtalha-babasab-jamadar <mdtalhajamadar@gmail.com>
2025-11-26 10:49:27 +0530

    Update addition.py

--- addition.py
+++ addition.py
@@ -1,7 +1,7 @@
 def add(a, b):
     return a + b
-
-if __name__ == "__main__":
-    x = 10
-    y = 20
-    print("sum:", add(x, y))
+import sys
+if _name_ == "_main_":
+    num1 = int(sys.argv[1])
+    num2 = int(sys.argv[2])
+    print("Sum:", add(num1, num2))
